0c528555aa9da8aa159849ef71fc93b3f96162cc
Davide Corradi <davide.corradi.dev@gmail.com>
2024-10-06 17:35:51 +0200

    Challenge: formulas.

--- src/main.c
+++ src/main.c
@@ -11,13 +11,36 @@ int main(int argc, const char *argv[])
     Chunk chunk;
     init_chunk(&chunk);
 
-    write_constant(&chunk, 1.2, 123);
-    write_constant(&chunk, 3.4, 123);
+    write_constant(&chunk, 1, 123);
+    write_constant(&chunk, 2, 123);
+    write_chunk(&chunk, OP_MULTIPLY, 123);
+    write_constant(&chunk, 3, 123);
     write_chunk(&chunk, OP_ADD, 123);
-    write_constant(&chunk, 5.6, 123);
-    write_chunk(&chunk, OP_DIVIDE, 123);
-    write_chunk(&chunk, OP_NEGATE, 123);
-    write_chunk(&chunk, OP_RETURN, 124);
+
+    write_constant(&chunk, 1, 124);
+    write_constant(&chunk, 2, 124);
+    write_constant(&chunk, 3, 124);
+    write_chunk(&chunk, OP_MULTIPLY, 124);
+    write_chunk(&chunk, OP_ADD, 124);
+
+    write_constant(&chunk, 3, 125);
+    write_constant(&chunk, 2, 125);
+    write_chunk(&chunk, OP_SUBTRACT, 125);
+    write_constant(&chunk, 1, 125);
+    write_chunk(&chunk, OP_SUBTRACT, 125);
+
+    write_constant(&chunk, 1, 126);
+    write_constant(&chunk, 2, 126);
+    write_constant(&chunk, 3, 126);
+    write_chunk(&chunk, OP_MULTIPLY, 126);
+    write_chunk(&chunk, OP_ADD, 126);
+    write_constant(&chunk, 4, 126);
+    write_constant(&chunk, 5, 126);
+    write_chunk(&chunk, OP_NEGATE, 126);
+    write_chunk(&chunk, OP_DIVIDE, 126);
+    write_chunk(&chunk, OP_SUBTRACT, 126);
+
+    write_chunk(&chunk, OP_RETURN, 127);
 
     printf("*** DISASSEMBLE ***\n");
     disassemble_chunk(&chunk, "test chunk");
